30c0f173fad4be4aaa6adcd4a80c627c3f7e00b3
IareDave <gouletdavid95@gmail.com>
2019-06-08 17:00:43 -0400

    Update upload_route

--- app/routes/upload_routes.js
+++ app/routes/upload_routes.js
@@ -2,7 +2,7 @@ const express = require('express')
 const Upload = require('../models/upload')
 const customErrors = require('../../lib/custom_errors')
 const handle404 = customErrors.handle404
-const removeBlanks = require('../../lib/remove_blank_fields')
+// const removeBlanks = require('../../lib/remove_blank_fields')
 const router = express.Router()
 const passport = require('passport')
 const requireToken = passport.authenticate('bearer', { session: false })
@@ -13,7 +13,7 @@ const multer = require('multer')
 
 // Configure the upload object telling multer where we want to store the image
 // temporarily on the server before sending it to aws.
-const upload = multer({ dest: 'uploads/', storage: multer.memoryStorage() })
+const uploadMult = multer({ dest: 'uploads/', storage: multer.memoryStorage() })
 // Require our promisified s3Upload function.
 const promiseS3Upload = require('../../lib/s3UploadApi.js')
 
@@ -22,7 +22,7 @@ const promiseS3Upload = require('../../lib/s3UploadApi.js')
 // In our POST route for /uploads we include the multer middleware.
 // The `single` method needs the name attribute from the form's input that has
 // a type of file
-router.post('/create-uploads', upload.single('image'), requireToken, (req, res, next) => {
+router.post('/create-uploads', uploadMult.single('image'), requireToken, (req, res, next) => {
   // req.body.upload.owner = req.user.id
   // Invoke our promisified s3Upload function, passing in the req.file which is
   // an object that multer attached to the request object.
@@ -69,16 +69,17 @@ router.get('/uploads/:id', (req, res, next) => {
 
 // UPDATE
 // PATCH /uploads/5a7db6c74d55bc51bdf39793
-router.patch('/uploads/:id', removeBlanks, requireToken, (req, res, next) => {
+router.patch('/uploads/:id', requireToken, (req, res, next) => {
+  // delete req.body.upload.owner
+
   Upload.findById(req.params.id)
     .then(handle404)
     .then(upload => {
-      // requireOwnership(req, upload)
+      // requireOwnership(req, upload)\
       return upload.update(req.body.upload)
     })
     .then(() => res.sendStatus(204))
     .catch(next)
-  // delete req.body.Upload.owner
   // promiseS3Upload(req.file)
   //   // This .then receives the response from aws if the upload was successful.
   //   .then(awsResponse => {
